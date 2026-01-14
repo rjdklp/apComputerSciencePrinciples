fa0b78fda4060ec498011f730561866b6cc11812
rjdklp <160688484+rjdklp@users.noreply.github.com>
2026-01-14 09:52:40 -0600

    Create traversalsMake.js

---
+++ traversalsMake.js
@@ -0,0 +1,49 @@
+var cityUnFiltered = getColumn("Daily Weather", "City");
+var highUnFiltered = getColumn("Daily Weather", "High Temperature");
+var lowUnFiltered = getColumn("Daily Weather", "Low Temperature");
+var conditionUnFiltered = getColumn("Daily Weather", "Condition Description");
+var iconUnFiltered = getColumn("Daily Weather", "Icon");
+
+var cityFiltered = [];
+var highFiltered = [];
+var lowFiltered = [];
+var conditionFiltered = [];
+var iconFiltered = [];
+
+var randNum = 0;
+
+function updateScreen(){
+  randNum = randomNumber(0, cityFiltered.length);
+  while(true){
+    setProperty("cityOutput", "text", cityFiltered[randNum]);
+    setProperty("highTempOutput", "text", highFiltered[randNum]);
+    setProperty("lowTempOutput", "text", lowFiltered[randNum]);
+    setProperty("conditionOutput", "text", conditionFiltered[randNum]);
+    setProperty("iconOutput", "image", iconFiltered[randNum]);
+    break;
+  }
+}
+
+onEvent("forecastButton", "click", function() {
+  updateScreen();
+});
+
+var dateUnfiltered = getColumn("Daily Weather", "Date");
+filterData();
+
+function filterData(){
+  console.log("filterData Activated");
+  for (var index; index<dateUnfiltered.length; index++){
+    if (dateUnfiltered[index] === "Thu 1/15"){
+      appendItem(cityFiltered, cityUnFiltered[index]);
+      appendItem(highFiltered, highUnFiltered[index]);
+      appendItem(lowFiltered, lowUnFiltered[index]);
+      appendItem(conditionFiltered, conditionUnFiltered[index]);
+      appendItem(iconFiltered, iconUnFiltered[index]);
+      console.log(dateUnfiltered);
+    } else{
+      console.log("continuned");
+      continue;
+    }
+  }
+}
