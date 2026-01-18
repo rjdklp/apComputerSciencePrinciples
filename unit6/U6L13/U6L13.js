f7037e42ba0be3776b0e85a997cb0e95642c7398
rjdklp <rjdklp.github@pm.me>
2026-01-17 22:02:08 -0600

    aura

--- unit6/U6L13/U6L13.js
+++ unit6/U6L13/U6L13.js
@@ -19,17 +19,37 @@ for(var index; index < indexLimit; index++){
 console.log("Finished fetching dataset in " + ((getTime() - datasetFetchInit)/1000) + " seconds and received " + (indexLimit + 1) + " entries \n");
 timeSinceInit();
 
-function centerX(desiredXPosition, elementWidth){
-    var truePosition = desiredXPosition - elementWidth/2;
-    return truePosition;
+function newLabel(desiredId, desiredMessage, desiredXPosition, desiredYPosition, elementWidth, elementHeight, fontSize){
+    if(typeof desiredId !== "string"){
+        throw new Error("desiredId must be a string");
+    } if(typeof desiredMessage !== "string"){
+        throw new Error("desiredMessage must be a string");
+    }
+    var trueXPosition = desiredXPosition - elementWidth/2;
+    var trueYPosition = desiredYPosition - elementHeight/2;
+    textLabel(desiredId, desiredMessage);
+    setPosition(desiredId, trueXPosition, trueYPosition, elementWidth, elementHeight);
+    setProperty(desiredId, "text-align", "center");
+    setProperty(desiredId, "font-size", fontSize);
 }
 
-function centerY(desiredYPosition, elementHeight){
-    var truePosition = desiredYPosition - elementHeight/2;
-    return truePosition;
+function newButton(desiredId, desiredMessage, desiredXPosition, desiredYPosition, elementWidth, elementHeight){
+    if(typeof desiredId !== "string"){
+        throw new Error("desiredId must be a string");
+    } if(typeof desiredMessage !== "string"){
+        throw new Error("desiredMessage must be a string");
+    }
+    var trueXPosition = desiredXPosition - elementWidth/2;
+    var trueYPosition = desiredYPosition - elementHeight/2;
+    button(desiredId, desiredMessage);
+    setPosition(desiredId, trueXPosition, trueYPosition, elementWidth, elementHeight);
+    setProperty(desiredId, "text-align", "center");
 }
 
-textLabel("s1Title", "Passwords Dataset Analysis");
-setPosition("s1Title", centerX(160, 300), centerY(50, 100), 300, 100);
-setProperty("s1Title", "font-size", 18);
-setProperty("s1Title", "text-align", "center");
+newLabel("screen1Title", "Passwords Dataset Analyzer", 160, 100, 300, 100, 25);
+newLabel("screen1Intro", "Made by rjdklp & Dom, please enjoy", 160, 225, 250, 150, 18);
+newButton("screen2NextScreenButton", "Next Screen", 260, 420, 100, 50);
+
+onEvent("screen2NextScreenButton", "click", function(){
+    setScreen("screen2");
+});
